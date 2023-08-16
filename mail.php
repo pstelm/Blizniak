e6a796e5770f18bf11e20d89832e923483291307
Paulina <paulina.bistula@gmail.com>
2023-08-16 16:38:35 +0200

    Change contact form mail styling

--- mail.php
+++ mail.php
@@ -6,37 +6,30 @@ $email = $_POST["email"];
 $phone = $_POST["phone"];
 $message = $_POST["message"];
 $subject = "Wiadomość ze strony blizniak-szklo.com";
-$to = "marcin@blizniak.com";
+// $to = "marcin@blizniak.com";
+$to = "plnstlmch@gmail.com";
 
 $txt = "Wiadomość od:  " . $name . "\r\n" . "Firma:  " . $company . "\r\n" . "Numer telefonu:  " . $phone . "\r\n" . "Email:  " . $email . "\r\n" . "\r\n" . "Treść wiadomości:  " . $message;
 
-// $txt = "<html>
-//         <head>
-//         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-//             <title></title>
-//         </head>
-//         <body>";
-// $txt .= "<p><span>Wiadomość od: </span style='color: #232561;>" . $name . "</p><br>";
-// $txt .= "<p><span>Firma: </span style='color: #232561;>" . $company . "</p><br>";
-// $txt .= "<p><span>Telefon: </span style='color: #232561;>" . $phone . "</p><br>";
-// $txt .= "<p><span>Wiadomość od: </span style='color: #232561;>" . $email . "</p><br>";
-// $txt .= "<p><span>Treść wiadomości: </span style='color: #232561;>" . $message . "</p><br>";
-
-// $txt .= "</body>";
-// $txt .= "</html>";
-
-// $headers = 'MIME-Version: 1.0' . "\r\n";
-// $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
+$txt_with_styling = '<html><body>';
+$txt_with_styling .= '<table rules="all" style="border: 1px solid; border-color: #ddd;" cellpadding="10">';
+$txt_with_styling .= '<tr><td style="background: #f2f2f2;"><strong>Wiadomość od:</strong> </td><td>' . $name . '</td></tr>';
+$txt_with_styling .= '<tr><td style="background: #f2f2f2;"><strong>Firma:</strong> </td><td>' . $company . '</td></tr>';
+$txt_with_styling .= '<tr><td style="background: #f2f2f2;"><strong>Email:</strong> </td><td>' . $email . '</td></tr>';
+$txt_with_styling .= '<tr><td style="background: #f2f2f2;"><strong>Numer telefonu:</strong> </td><td>' . $phone . '</td></tr>';
+$txt_with_styling .= '<tr><td style="background: #f2f2f2;"><strong>Treść wiadomości:</strong> </td><td>' . $message . '</td></tr>';
+$txt_with_styling .= '</table>';
+$txt_with_styling .= '</body></html>';
 
 $headers .= "From: " . $email . "\r\n";
 $headers .= "Reply-To: " . $email . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-$mail_status = mail($to, $subject, $txt, $headers);
+$mail_status = mail($to, $subject, $txt_with_styling, $headers);
 
 if ($mail_status) {
   header("Location: /index.html?mail_status=sent");
 } else {
   header("Location: /index.html?mail_status=error");
 }
-
